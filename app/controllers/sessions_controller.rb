0dbc89e6705ac33d9f495adf615474c4d8c0b1de
waasifkhaan <waasif.khaan@gmail.com>
2018-10-23 16:11:47 -0500

    work on layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,11 @@ class SessionsController < ApplicationController
     end
     
     def create
+        # binding.pry
         if logged_in?
           redirect_to user_path(session[:user_id])
         else
-          @user = User.find_by(name: params[:user_name])
+          @user = User.find_by(email: params[:email])
           if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
