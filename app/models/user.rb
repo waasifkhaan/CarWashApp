af75c8f842c606496b2a336aa8809c941b26a62f
waasifkhaan <waasif.khaan@gmail.com>
2018-10-23 22:13:46 -0500

    work on layout background

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   has_many :carwashes
   has_many :detailers, through: :carwashes
   has_secure_password
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true
 
 end
