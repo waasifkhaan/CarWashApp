d64c7fe9b1b996802714a8759a53e7e71120cb60
waasifkhaan <waasif.khaan@gmail.com>
2018-10-24 20:08:07 -0500

    build carwashes on user in the nested routes

--- app/controllers/carwashes_controller.rb
+++ app/controllers/carwashes_controller.rb
@@ -1,9 +1,15 @@
 class CarwashesController < ApplicationController
 
     def new 
-        @carwash = Carwash.new
+        binding.pry
+        user = User.find(params[:user_id])
+        user.carwashes.build
+        @carwash= Carwash.new
+        @detailers = Detailer.all
     end 
     def index
-        @detailers = Detailer.all 
+      
     end 
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end 
   resources :cars
   resources :detailers
-  resources :carwashes
+  
   resources :sessions
   
   get 'login' => 'sessions#new'
