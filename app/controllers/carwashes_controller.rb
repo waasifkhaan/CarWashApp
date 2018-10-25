39bb607000c50764d4b269a369f757900e87414a
waasifkhaan <waasif.khaan@gmail.com>
2018-10-25 14:00:03 -0500

    carwash show method in place

--- app/controllers/carwashes_controller.rb
+++ app/controllers/carwashes_controller.rb
@@ -5,15 +5,43 @@ class CarwashesController < ApplicationController
         user = User.find(params[:user_id])
         @carwash = user.carwashes.build
         @detailers = Detailer.all
+        @cars = Car.all
         
     end 
     
-    def index
+    def show
+        
+        @carwash = Carwash.find(params[:id])
+        
       
     end 
     def create 
+              
+        carwash = Carwash.new
+        if valid
+            carwash.user_id = params[:user_id]
+            carwash.car_id = params[:carwash][:car_id][1]
+            carwash.detailer_id = params[:carwash][:detailer_id][1]
+            carwash.save
+            redirect_to user_carwash_path(params[:user_id], carwash)
+        else 
+            redirect_to new_user_carwash_path
+        end 
+        
         
     end 
+
+    private
+
+    def carwash_params
+      params.require(:carwash).permit(:car_id[1], :detailer_id[1])
+    end
+
+    def valid
+        params[:carwash][:car_id][1] && params[:carwash][:detailer_id][1]
+    end 
+  
+  
 
 
 end
--- app/models/carwash.rb
+++ app/models/carwash.rb
@@ -1,7 +1,7 @@
 class Carwash < ApplicationRecord
   belongs_to :detailer
   belongs_to :user
-  belongs_to :car
+
 
 
   def readable_time 
