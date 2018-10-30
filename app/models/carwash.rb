436b1cec94b347b25baa143a4d967cfa4983a31d
waasifkhaan <waasif.khaan@gmail.com>
2018-10-29 23:09:15 -0500

    add conditional validation in carwash model

--- app/models/carwash.rb
+++ app/models/carwash.rb
@@ -4,11 +4,16 @@ class Carwash < ApplicationRecord
   belongs_to :car
   
   scope :uncommented, -> { where(user_comment: [nil, '']) }
-  
-  
-  
   validates :user_rating, numericality: { less_than_or_equal_to: 5 }
   
+  with_options if: :has_id? do |carwash|
+    carwash.validates :user_comment, presence: true
+    carwash.validates :user_rating, presence: true
+  end
+
+  def has_id?
+    self.id ? true : false
+  end 
 
   
 
