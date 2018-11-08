53e03f0a415f74206920a6ab60f96bbd324ec151
waasifkhaan <waasif.khaan@gmail.com>
2018-11-08 09:48:44 -0600

    removed pry :)

--- app/controllers/carwashes_controller.rb
+++ app/controllers/carwashes_controller.rb
@@ -31,7 +31,7 @@ class CarwashesController < ApplicationController
     end 
     
     def update 
-        binding.pry
+       
        
         @carwash = Carwash.find(params[:id])
              
